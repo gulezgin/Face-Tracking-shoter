a34eb19c6d761edbcbfd46d64effe63dc0961584
Tarık Gülezgin <81231546+gulezgin@users.noreply.github.com>
2024-11-18 23:39:25 +0300

    Update tracking_code.py

--- tracking_code.py
+++ tracking_code.py
@@ -1,78 +1,86 @@
- __author__ = "Brindal Patel"
-## this program is based on the haar_cascade classifier which are based on  Viola and Jones algorithm
-## this program is creating database of the detected face during real time
-
-## Used microcontroller board: ""Arduino Uno""
-
-
+## Bu program, Viola ve Jones algoritmasına dayalı Haar Cascade sınıflandırıcısını kullanır
+## Gerçek zamanlı olarak algılanan yüzlerin veritabanını oluşturur
+## Kullanılan mikrodenetleyici kartı: "Arduino Uno"
 
 import numpy as np
 import cv2
 import serial
 
-ser = serial.Serial(port='COM5',baudrate=9600,timeout=1)
-# this is for the opening communication between computer and Arduino Uno using serial portion
-# make sure to check your COM-Port number, if it is different change it in above line.
-# If you are using different baudrate than edit according to that
+# Seri iletişim ayarları
+ser = serial.Serial(port='COM5', baudrate=9600, timeout=1)
 
-#This function can be used to send serial data to Arduino Uno to do something
+# Servo motoru döndürmek için seri veri gönderme fonksiyonu
 def servo_rotate_serialdata(motornum, AngleServo):
-    ser.write(chr(255))
-    ser.write(chr(motornum))
-    ser.write(chr(AngleServo))
+    ser.write(chr(255).encode())
+    ser.write(chr(motornum).encode())
+    ser.write(chr(AngleServo).encode())
 
+# Algılanan yüzün etrafına kutu çizen fonksiyon
+def create_box(a, b, c, d):
+    cv2.rectangle(imag, (a, b), (a + c, b + d), (0, 255, 0), 2)
 
-#this function will create the box arround the detcted face
-def create_box(a,b,c,d):
-    cv2.rectangle(imag,(a,b),(a+c,b+d),(0,255,0),2)
+# Sayacı artıran fonksiyon
+def variable_iteration(l):
+    return l + 1
 
-def variable_itteration(l):
-    l=l+1
-    return(l)
-
+# Algılanan yüz kısmını kaydeden fonksiyon
 def write_image():
-    save_image= imag[y:y+h, x:x+w] #cut out the face portion from image
-    cv2.imwrite("hey%d.png"%(n),save_image)
+    face_image = imag[y:y+h, x:x+w]  # Yüz kısmını kes
+    cv2.imwrite(f"face_{n}.png", face_image)
 
+# Servo motor açısını kontrol eden fonksiyon
 def angle_servo(angle):
-    #prov=90
-    if angle>230:
-        prov=2
-        servo_rotate_serialdata(1,prov)
+    if angle > 230:
+        prov = 2  # Servo motoru sağa döndür
+        servo_rotate_serialdata(1, prov)
+    elif angle < 195:
+        prov = 1  # Servo motoru sola döndür
+        servo_rotate_serialdata(1, prov)
 
-    elif angle<195:
-        prov=1
-        servo_rotate_serialdata(1,prov)
+# Haar Cascade sınıflandırıcıyı yükle
+face_cascade = cv2.CascadeClassifier(r'C:\Python27\libs\haarcascade_frontalface_default.xml')
 
+# Video yakalama aygıtını başlat
+video_capture = cv2.VideoCapture(0)
 
-# this is usual path for windows user. If you are not able to find a xml file on your machine or online, check the github repository.
-face_casc = cv2.CascadeClassifier('C:\Python27\libs\haarcascade_frontalface_default.xml')
+# Dosya isimlendirme için başlangıç numarası
+n = 0
 
-videoWeb = cv2.VideoCapture(0)
-
-n=0
-
-while (videoWeb.isOpened()):
-
-    ret,imag = videoWeb.read()
-    #cv2.imshow('xyz',imag)
-    faces = face_casc.detectMultiScale(imag, 1.2, 5, minSize=(10,10),maxSize=(500,500))
-    for (x,y,w,h) in faces:
-        create_box(x,y,w,h)
-        n=variable_itteration(n)
-        # If you want to save all the detected face,then uncomment the following line
-        # write_image()
-
-        # calling function to send serial data of angle to rotate servo motor
-        angle_servo(x)
-
-    cv2.imshow('image',imag)
-
-    # to add exit key
-    k=cv2.waitKey(10)
-    # 27 is for the escape key, you can easily google the number and switchwith other keys
-    if k==27:
+while video_capture.isOpened():
+    ret, imag = video_capture.read()
+    if not ret:
+        print("Kameradan görüntü alınamadı. Program sonlandırılıyor.")
         break
 
-cv2.waitKey(0)
+    # Yüz algılama
+    faces = face_cascade.detectMultiScale(
+        imag, 
+        scaleFactor=1.2, 
+        minNeighbors=5, 
+        minSize=(10, 10), 
+        maxSize=(500, 500)
+    )
+
+    # Algılanan her yüz için işlemler
+    for (x, y, w, h) in faces:
+        create_box(x, y, w, h)       # Kutuyu çiz
+        n = variable_iteration(n)   # Sayacı artır
+
+        # Eğer algılanan yüzlerin kaydedilmesi isteniyorsa, aşağıdaki satırı aktif edin
+        # write_image()
+
+        # Servo motor açısını kontrol etmek için fonksiyonu çağır
+        angle_servo(x)
+
+    # Görüntüyü ekranda göster
+    cv2.imshow('Detected Faces', imag)
+
+    # Çıkış için 'ESC' tuşu kontrolü
+    k = cv2.waitKey(10)
+    if k == 27:  # ESC tuşu
+        print("Program sonlandırılıyor.")
+        break
+
+# Kaynakları serbest bırak ve pencereleri kapat
+video_capture.release()
 cv2.destroyAllWindows()
